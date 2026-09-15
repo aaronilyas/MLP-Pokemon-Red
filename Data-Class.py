2d23508461bf8e4c683768f4ba01fc14abdf7ea0
aaronilyas <aaronilyas@hotmail.com>
2026-09-14 20:56:56 -0500

    Added the ability for the __init__ method to load the contents of each line in a list, next step is to have it only load image names.

--- Data-Class.py
+++ Data-Class.py
@@ -1,16 +1,34 @@
-import json
-class Data_From_JsonL():
+from json import load, loads
+from numpy import array
+import numpy.typing as npt
+
+
+class Data_From_JsonL:
     file_path: str
     number_of_lines: int
+    array_of_images: npt.ArrayLike
     image_input_pairs: dict
 
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
         self.number_of_lines = 0
-        self.image_input_pairs = dict()
-        with open(file_path) as data:
+        self.image_input_pairs = {}
+        self.array_of_images = []
+        with open(file_path, "r") as data:
             for line in data:
-               self.number_of_lines += 1
+                self.number_of_lines += 1
+
+            for i in range(self.number_of_lines):
+                self.array_of_images.insert(i, 0)
+
+            data.close()
+
+        with open(file_path, "r") as data_again:
+            for line_two in data_again:
+                if line_two.strip():
+                    line_value = loads(line_two)
+                    self.array_of_images.append(line_value)
+            data_again.close()
 
     def get_number_of_lines(self) -> int:
         return self.number_of_lines
@@ -18,12 +36,13 @@ class Data_From_JsonL():
     def get_image_input_pairs(self) -> dict:
         return self.image_input_pairs
 
+    def get_array_of_images(self) -> npt.ArrayLike:
+        return self.array_of_images
 
 
 def main():
     data = Data_From_JsonL("steps.jsonl")
-    print(data.get_image_input_pairs().values())
-    print(data.get_number_of_lines())
+    print(data.get_array_of_images())
 
 
 if __name__ == "__main__":
