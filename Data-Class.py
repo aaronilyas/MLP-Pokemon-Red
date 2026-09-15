a13a5e6a54d2d2ef869e974e7591685caecb450c
aaronilyas <aaronilyas@hotmail.com>
2026-09-15 16:52:57 -0500

    Added the ability for the data class to load each of the key value pairs to a list.

--- Data-Class.py
+++ Data-Class.py
@@ -1,48 +1,48 @@
 from json import load, loads
-from numpy import array
-import numpy.typing as npt
+from torch import tensor
+from PIL import Image
 
 
 class Data_From_JsonL:
     file_path: str
     number_of_lines: int
-    array_of_images: npt.ArrayLike
-    image_input_pairs: dict
+    list_of_dictionaries: list
 
     def __init__(self, file_path: str) -> None:
         self.file_path = file_path
         self.number_of_lines = 0
-        self.image_input_pairs = {}
-        self.array_of_images = []
+        self.list_of_dictionaries = []
         with open(file_path, "r") as data:
             for line in data:
                 self.number_of_lines += 1
-
-            for i in range(self.number_of_lines):
-                self.array_of_images.insert(i, 0)
 
             data.close()
 
         with open(file_path, "r") as data_again:
             for line_two in data_again:
                 if line_two.strip():
-                    line_value = loads(line_two)
-                    self.array_of_images.append(line_value)
+                    line_value = line_two
+                    line_value = dict(loads(line_value))
+                    self.list_of_dictionaries.append(line_value)
             data_again.close()
 
     def get_number_of_lines(self) -> int:
         return self.number_of_lines
 
-    def get_image_input_pairs(self) -> dict:
-        return self.image_input_pairs
+    def get_list_of_dictionaries(self) -> list:
+        return self.list_of_dictionaries
 
-    def get_array_of_images(self) -> npt.ArrayLike:
-        return self.array_of_images
+    def get_tensor_of_gray_scale_pixel_values_from_list(self, index):
+        image_name = self.list_of_dictionaries[index]["image"]
+        image_name = "first_gym_001/" + image_name
+        img = Image.open(image_name)
+        gray_img = img.convert("L")
+        img_data = tensor(list(gray_img.get_flattened_data()))  # type: ignore
+        return img_data
 
 
 def main():
     data = Data_From_JsonL("steps.jsonl")
-    print(data.get_array_of_images())
 
 
 if __name__ == "__main__":
