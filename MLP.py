bb1da672c0a221912b3f845b22bc66e484bbbe2d
aaronilyas <aaronilyas@hotmail.com>
2026-09-18 12:40:17 -0500

    MLP class now save weights and biases to a .pth file as well as load them. Moreover, I also added a method that allows the model to be used during inference.

--- MLP.py
+++ MLP.py
@@ -1,9 +1,9 @@
-from typing import List
-
-from torch import Tensor, dropout, optim, tensor
+from torch import Tensor, argmax, dropout, optim, tensor
 import torch
 import torch.nn as nn
 import Data_Class
+
+
 class MLP(nn.Module):
     data_set: Data_Class.Data_From_JsonL
     input_value: Tensor
@@ -14,63 +14,72 @@ class MLP(nn.Module):
     def __init__(self, data_set: Data_Class.Data_From_JsonL) -> None:
         super().__init__()
         self.data_set = data_set
-        self.input_value = self.data_set.get_tensor_of_gray_scale_pixel_values_and_its_dictionary(0)[0]
-        self.number_of_activations_for_hidden_layer = len(self.input_value)
-        self.model = nn.Sequential(
-            nn.Linear(self.number_of_activations_for_hidden_layer,self.number_of_activations_for_hidden_layer),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.ReLU(),
-            nn.Dropout(),
+        self.input_value = (
+            self.data_set.get_tensor_of_gray_scale_pixel_values_and_its_dictionary(0)[0]
         )
-        self.optimizer = optim.Adam(self.model.parameters(),lr=0.01)
+        self.number_of_activations_for_input_layer = len(self.input_value)
 
-    def relate_input_with_number(self, input_button: str)-> int:
-        if(len(input_button) <= 0):
+        self.model = nn.Sequential(
+            nn.Linear(self.number_of_activations_for_input_layer, 32),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Softmax(),
+        )
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.01)
+
+    def relate_input_with_number(self, input_button: str) -> int:
+        if len(input_button) <= 0:
             return 9
-        if(input_button[0] == "UP"):
+        if input_button[0] == "UP":
             return 1
-        if(input_button[0] == "DOWN"):
+        if input_button[0] == "DOWN":
             return 2
-        if(input_button[0] == "LEFT"):
+        if input_button[0] == "LEFT":
             return 3
-        if(input_button[0] == "RIGHT"):
+        if input_button[0] == "RIGHT":
             return 4
-        if(input_button[0] == "A"):
+        if input_button[0] == "A":
             return 5
-        if(input_button[0] == "B"):
+        if input_button[0] == "B":
             return 6
-        if(input_button[0] == "START"):
+        if input_button[0] == "START":
             return 7
-        if(input_button[0] == "END"):
+        if input_button[0] == "END":
             return 8
         else:
             return 9
 
-    def find_input_from_number(self, input_number: int)->str:
-        if(input_number == 1):
+    def find_input_from_number(self, input_number: int) -> str:
+        if input_number == 1:
             return "UP"
-        if(input_number == 2):
+        if input_number == 2:
             return "DOWN"
-        if(input_number == 3):
+        if input_number == 3:
             return "LEFT"
-        if(input_number == 4):
+        if input_number == 4:
             return "RIGHT"
-        if(input_number == 5):
+        if input_number == 5:
             return "A"
-        if(input_number == 6):
+        if input_number == 6:
             return "B"
-        if(input_number == 7):
+        if input_number == 7:
             return "START"
-        if(input_number == 8):
+        if input_number == 8:
             return "END"
         else:
             return "NONE"
+
     def train_network(self, number_of_epochs: int) -> None:
         criterion = nn.CrossEntropyLoss()
 
         for epoch in range(0, number_of_epochs, 37):
-            pixels, label_info = self.data_set.get_tensor_of_gray_scale_pixel_values_and_its_dictionary(epoch)
+            pixels, label_info = (
+                self.data_set.get_tensor_of_gray_scale_pixel_values_and_its_dictionary(
+                    epoch
+                )
+            )
             input_data = pixels.float() / 255.0
 
             expected_outputs = self.relate_input_with_number(label_info["inputs"])
@@ -85,16 +94,35 @@ class MLP(nn.Module):
             loss.backward()
             self.optimizer.step()
 
-            print("Loss:", float(loss), "Prediction:", prediction.detach(), "Expected:", self.find_input_from_number(expected_outputs))
+            """print(
+                "Loss:",
+                float(loss),
+                "Prediction:",
+                prediction.detach(),
+                "Expected:",
+                self.find_input_from_number(expected_outputs),
+            )"""
 
+    def load_models_weights_and_biases(self) -> None:
+        self.model.load_state_dict(torch.load("models_weights_and_biases.pth"))
 
+    def use_network(self, input_data: torch.Tensor) -> str:
+        if self.model.training:
+            self.model.eval()
+        with torch.no_grad():
+            input_data = input_data.to(torch.float32)
+            models_output = max(self.model(input_data))
+            return self.find_input_from_number(models_output)
 
+    def save_weights_and_biases(self) -> None:
+        torch.save(self.model.state_dict(), "models_weights_and_biases.pth")
 
 
 def main():
-    data = Data_Class.Data_From_JsonL("/Users/aaronilyas/MLP-Pokemon-Red/steps.jsonl")
+    data = Data_Class.Data_From_JsonL("steps.jsonl")
     mlp_1 = MLP(data)
-    mlp_1.train_network(13542)
+    # mlp_1.train_network(13542)
+    # data = data.get_tensor_of_gray_scale_pixel_values_and_its_dictionary(0)[0]
 
 
 if __name__ == "__main__":
